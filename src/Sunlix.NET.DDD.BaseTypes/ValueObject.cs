0b7e152752822900b6a88e6d8329635f5ff3cae2
igor-bzovsky <igor.bzovsky@gmail.com>
2025-08-09 17:47:17 +0300

    Added xml docs for ValueObject class

--- src/Sunlix.NET.DDD.BaseTypes/ValueObject.cs
+++ src/Sunlix.NET.DDD.BaseTypes/ValueObject.cs
@@ -1,9 +1,14 @@
 ﻿namespace Sunlix.NET.DDD.BaseTypes
 {
+    /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='T:ValueObject']/*" />
     public abstract class ValueObject : IEquatable<ValueObject>
     {
         private int? _cachedHashCode;
 
+        /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='P:UnproxiedType']/*" />
+        protected virtual Type UnproxiedType => GetType();
+
+        /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='M:Equals(ValueObject)']/*" />
         public bool Equals(ValueObject? other)
         {
             if (other is null) return false;
@@ -12,9 +17,11 @@
             return SatisfiesStructuralEquality(other);
         }
 
+        /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='M:Equals(System.Object)']/*" />
         public override bool Equals(object? obj)
             => obj is ValueObject other && Equals(other);
 
+        /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='M:GetHashCode']/*" />
         public override int GetHashCode()
         {
             if (_cachedHashCode.HasValue) return _cachedHashCode.Value;
@@ -29,14 +36,14 @@
             return _cachedHashCode.Value;
         }
 
+        /// <include file="XmlDocs/ValueObject.xml" path="doc/members/member[@name='M:GetEqualityComponents']/*" />
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
         public static bool operator ==(ValueObject? left, ValueObject? right)
             => left is null ? right is null : left.Equals(right);
+
         public static bool operator !=(ValueObject? left, ValueObject? right)
             => !(left == right);
-
-        protected virtual Type UnproxiedType => GetType();
-
-        protected abstract IEnumerable<object> GetEqualityComponents();
 
         private bool SatisfiesStructuralEquality(ValueObject other)
             => GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
