8cc3666828c0ca2913d4e411b2dc4a81a5f1c093
igor-bzovsky <igor.bzovsky@gmail.com>
2025-08-09 16:31:29 +0300

    Added xml docs for Entity class

--- src/Sunlix.NET.DDD.BaseTypes/Entity.cs
+++ src/Sunlix.NET.DDD.BaseTypes/Entity.cs
@@ -1,20 +1,35 @@
 ﻿namespace Sunlix.NET.DDD.BaseTypes
 {
+    /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='T:Entity']/*" />
     public abstract class Entity<TId> where TId : IEquatable<TId>
     {
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='M:Entity.#ctor']/*" />
         protected Entity() { }
-        protected Entity(TId id) => Id = id;
+
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='M:Entity.#ctor(`0)']/*" />
+        protected Entity(TId id)
+        {
+            if (EqualityComparer<TId>.Default.Equals(id, default!))
+                throw new ArgumentException("Entity Id should not be null or default value.", nameof(id));
+
+            Id = id;
+        }
+
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='P:Entity.Id']/*" />
         public virtual TId? Id { get; protected set; }
 
-        public virtual bool IsTransient => EqualityComparer<TId>.Default.Equals(Id, default);
-
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='P:UnproxiedType']/*" />
         protected virtual Type UnproxiedType => GetType();
 
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='P:IdEqualityComparer']/*" />
         public static IEqualityComparer<Entity<TId>> IdEqualityComparer =>
             EqualityComparer<Entity<TId>>.Create((x, y) =>
             x is null ? y is null : y is not null
             && x.UnproxiedType == y.UnproxiedType
             && SatisfiesIdentifierEquality(x.Id, y.Id));
+
+        /// <include file="XmlDocs/Entity.xml" path="doc/members/member[@name='M:Entity.IsTransient']/*" />
+        public virtual bool IsTransient() => EqualityComparer<TId>.Default.Equals(Id, default);
 
         private static bool SatisfiesIdentifierEquality(TId? id, TId? otherId) =>
             id is null ? otherId is null : id.Equals(otherId);
--- tests/Sunlix.NET.DDD.BaseTypes.Tests/EntityTests.cs
+++ tests/Sunlix.NET.DDD.BaseTypes.Tests/EntityTests.cs
@@ -3,21 +3,43 @@
     public class EntityTests
     {
         [Theory]
-        [MemberData(nameof(GetEntityWithResultTuples))]
+        [MemberData(nameof(GetValidEntitiesWithIds))]
         public void Entity_with_defined_id_should_be_initialized_correctly<TId>(TestEntity<TId> sut, TId id)
             where TId : IEquatable<TId>
         {
             sut.Id.Should().Be(id);
-            sut.IsTransient.Should().BeFalse();
+            sut.IsTransient().Should().BeFalse();
+        }
+
+        [Fact]
+        public void Constructor_should_throw_exception_when_id_is_null()
+        {
+            Action act = () => new TestEntity<string>(null!);
+
+            act.Should()
+               .Throw<ArgumentException>()
+               .WithMessage("Entity Id should not be null or default value.*")
+               .And.ParamName.Should().Be("id");
+        }
+
+        [Fact]
+        public void Constructor_should_throw_exception_when_id_is_default()
+        {
+            Action act = () => new TestEntity<int>(0);
+
+            act.Should()
+               .Throw<ArgumentException>()
+               .WithMessage("Entity Id should not be null or default value.*")
+               .And.ParamName.Should().Be("id");
         }
 
         [Theory]
-        [MemberData(nameof(GetTransientEntityWithResultTuples))]
+        [MemberData(nameof(GetTransientEntitiesWithIds))]
         public void Transient_entity_should_be_initialized_correctly<TId>(TestEntity<TId> sut, TId id)
             where TId : IEquatable<TId>
         {
             sut.Id.Should().Be(id);
-            sut.IsTransient.Should().BeTrue();
+            sut.IsTransient().Should().BeTrue();
         }
 
         public class TestEntity<TId> : Entity<TId> where TId : IEquatable<TId>
@@ -26,18 +48,22 @@
             public TestEntity(TId id) : base(id) { }
         }
 
-        public static IEnumerable<object[]> GetEntityWithResultTuples()
+        public static IEnumerable<object[]> GetValidEntitiesWithIds()
         {
             yield return new object[] { new TestEntity<int>(1), 1 };
             yield return new object[] { new TestEntity<string>("ae61412c-0bab-47f0-82f7-39520ede5639"), "ae61412c-0bab-47f0-82f7-39520ede5639" };
         }
 
-        public static IEnumerable<object[]> GetTransientEntityWithResultTuples()
+        public static IEnumerable<object[]> GetDefaultIds()
+        {
+            yield return new object[] { 0 };
+            yield return new object[] { (string)null! };
+        }
+
+        public static IEnumerable<object[]> GetTransientEntitiesWithIds()
         {
             yield return new object[] { new TestEntity<int>(), 0 };
             yield return new object[] { new TestEntity<string>(), null! };
-            yield return new object[] { new TestEntity<int>(0), 0 };
-            yield return new object[] { new TestEntity<string>(null!), null! };
         }
 
     }
